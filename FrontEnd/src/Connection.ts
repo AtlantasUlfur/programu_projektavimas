d1cea448fcd80558cdefa5e1d2f70ece8455d8ff
Lisajus <justas.lisajus@ktu.edu>
2023-10-18 00:03:34 +0300

    feat: main menu

---
+++ FrontEnd/src/Connection.ts
@@ -0,0 +1,27 @@
+import { io, Socket } from 'socket.io-client';
+
+//Singleton
+export class Connection
+{
+    private static _instance : Connection | null = null;
+    private socket : Socket | null = null;
+
+    private constructor(){}
+
+    public static getInstance(): Connection {
+        if (!Connection._instance) {
+            Connection._instance = new Connection();
+        }
+        return Connection._instance;
+    }
+    
+    public connect(url: string): void {
+        if (!this.socket) {
+            this.socket = io(url);
+        }
+    }
+
+    public getSocket(): Socket | null {
+        return this.socket;
+    }
+}
--- FrontEnd/src/game.ts
+++ FrontEnd/src/game.ts
@@ -1,35 +1,22 @@
 import * as Phaser from 'phaser';
+import { config } from '../config/Config'
+import { MainMenuScene } from './scenes/MainMenuScene';
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
+var MAIN_MENU_KEY = "MainMenu";
+
+class Game extends Phaser.Game {
+
+    constructor()
     {
-        super('demo');
+        super(config);
+
+        this.scene.add(MAIN_MENU_KEY, MainMenuScene);
+
+        this.scene.start(MAIN_MENU_KEY);
     }
 
-    preload ()
-    {
-
-    }
-
-    create ()
-    {
-
-
-
-    }
 }
 
-const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#00000',
-    width: 1280,
-    height: 600,
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    scene: Demo
-};
-
-const game = new Phaser.Game(config);
+window.onload = function() {
+    window.game = new Game();
+}
