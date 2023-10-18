f7899efaa9fb5c476a4f57c5246800ceed01fd5c
Edmundas Danėnas <edmundas.danenas@ktu.edu>
2023-10-18 17:21:54 +0300

    fix template

---
+++ FrontEnd/src/scripts/Models/Button.ts
@@ -0,0 +1,11 @@
+import * as Phaser from "phaser";
+
+export class Button {
+    public Image : Phaser.GameObjects.Image;
+    public Text : Phaser.GameObjects.Text
+
+    constructor(image : Phaser.GameObjects.Image, text : Phaser.GameObjects.Text){
+        this.Image = image;
+        this.Text = text;
+    }
+}
