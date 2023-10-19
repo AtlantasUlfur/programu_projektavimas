cd8b3070324864aa866a6f2ce6bf490c76f79e77
Edmundas Danėnas <edmundas.danenas@ktu.edu>
2023-10-19 16:51:01 +0300

    more player methods

--- FrontEnd/src/scripts/Models/Player.ts
+++ FrontEnd/src/scripts/Models/Player.ts
@@ -1,37 +1,90 @@
-import { SizeEnum } from "./Enums";
+import { SizeEnum } from './Enums'
 export class Player extends Phaser.GameObjects.Sprite {
-    private playerName : Phaser.GameObjects.Text;
-    constructor(scene : Phaser.Scene, x : number, y : number, key : string){
-        super(scene, 0, 0, key);
-        
-        //Add to scene
-        this.scene.add.existing(this);
+  private playerName: Phaser.GameObjects.Text
+  private isMoved: boolean
+  private isFinished: boolean
+  private currentHealth: integer
+  private waitText: Phaser.GameObjects.Text
 
-        //Self explanitory 
-        this.setTexture("player");
+  constructor(scene: Phaser.Scene, x: number, y: number, key: string) {
+    super(scene, 0, 0, key)
 
-        this.setDepth(0);
+    this.currentHealth = 100
+    this.isMoved = false
+    this.isFinished = false
 
-        this.setPosition(x, y);
+    //Add to scene
+    this.scene.add.existing(this)
 
-        this.playerName = this.scene.add.text(0, 0, "YOU", {
-            fontFamily: "Arial",
-            fontSize: "16",
-            fontStyle: "bold",
-            color: "#008000",
-        });
-        this.playerName.setDepth(10);
-        this.playerName.setOrigin(0.5, 1.5);
-        this.playerName.setResolution(7);
-    }
+    //Self explanitory
+    this.setTexture('player')
 
-    update(time : number, delta : number){
-        this.showPlayerNickname();
-    }
+    this.setDepth(0)
 
-    showPlayerNickname() {
-        // this.playerName.x = this.x - (this.playerName.width / 2);
-        // this.playerName.y = this.y - (this.height / 2);
-        this.playerName.setPosition(this.x, this.y - SizeEnum.PLAYER_NAME_OFFSET)
-    }
+    this.setPosition(x, y)
+
+    this.playerName = this.scene.add.text(0, 0, 'YOU', {
+      fontFamily: 'Arial',
+      fontSize: '16',
+      fontStyle: 'bold',
+      color: '#008000'
+    })
+    this.playerName.setDepth(10)
+    this.playerName.setOrigin(0.5, 1.5)
+    this.playerName.setResolution(7)
+  }
+
+  update(time: number, delta: number) {
+    this.showPlayerNickname()
+  }
+
+  showPlayerNickname() {
+    // this.playerName.x = this.x - (this.playerName.width / 2);
+    // this.playerName.y = this.y - (this.height / 2);
+    this.playerName.setPosition(this.x, this.y - SizeEnum.PLAYER_NAME_OFFSET)
+  }
+
+  setMoveStatus(isMoved: boolean) {
+    this.isMoved = isMoved
+  }
+
+  isAvailable(): boolean {
+    return !this.isFinished
+  }
+
+  onDamage(damage: integer) {
+    this.currentHealth -= damage
+  }
+
+  isDead(): boolean {
+    return this.currentHealth <= 0
+  }
+
+  createFinishedText() {
+    this.waitText = this.scene.add
+      .text(this.x, this.y, 'E', {
+        padding: {
+          left: 10,
+          top: 10
+        }
+      })
+      .setVisible(false)
+  }
+
+  setFinishedText() {
+    this.waitText.setX(this.x)
+    this.waitText.setY(this.y)
+    this.waitText.setVisible(true)
+  }
+
+  finishAction() {
+    this.setMoveStatus(false)
+    this.isFinished = true
+    this.setFinishedText()
+  }
+
+  startAction() {
+    this.isFinished = false
+    this.waitText.setVisible(false)
+  }
 }
