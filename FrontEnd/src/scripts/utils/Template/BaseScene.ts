deaa3bbade08b6f81c129fd7366c74289b4735bc
Edmundas Danėnas <edmundas.danenas@ktu.edu>
2023-12-01 04:00:19 +0200

    fix smth

--- FrontEnd/src/scripts/utils/Template/BaseScene.ts
+++ FrontEnd/src/scripts/utils/Template/BaseScene.ts
@@ -4,14 +4,14 @@ import SocketController from "../../SocketController"
 
 export abstract class BaseScene extends Phaser.Scene{
     //Utils
-    protected socketInstance: SocketController
+    public socketInstance: SocketController
     
     protected init(data?: any):void {
         this.socketInstance = SocketController.getInstance()
         this.socketInstance.setScene(this)
     } 
-    protected preload():void {} //hook
-    protected create():void {} //hook
+    public preload():void {} //hook
+    public create():void {} //hook
     abstract update(time? : number, delta?: number) : void; //must be implemented
 
 }
