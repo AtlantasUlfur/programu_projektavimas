4c32277799e35dc6de92be93216d8a6d3461f508
Lisajus <justas.lisajus@ktu.edu>
2023-12-08 04:03:00 +0200

    feat: add time travel item

--- FrontEnd/src/scripts/utils/Visitor/Visitor.ts
+++ FrontEnd/src/scripts/utils/Visitor/Visitor.ts
@@ -1,24 +1,70 @@
-import { HealthItem } from "../../Models/Items";
+import { HealthItem, RemoveBleedingItem, TimeTravelItem } from "../../Models/Items";
 import SocketController from "../../SocketController";
 
 export interface IVisitor {
-    useHealthItem(item: HealthItem, socketInstance : SocketController): void
+    useHealthItem(item: HealthItem): void
+    useRemoveBleedingItem(item: RemoveBleedingItem): void
+    useTimeTravelItem(item: TimeTravelItem): void
 }
 
 export class ItemVisitor implements IVisitor{
-    public useHealthItem(item : HealthItem, socketInstance : SocketController) : void {
-        socketInstance.healPlayer(item.useEffect());
+    private socketInstance : SocketController;
+    private sceneEvents : Phaser.Events.EventEmitter;
+
+    constructor(socketInstance : SocketController, sceneEvents : Phaser.Events.EventEmitter){
+        this.socketInstance = socketInstance;
+        this.sceneEvents = sceneEvents;
+    }
+
+    public useHealthItem(item : HealthItem) : void {
+        this.socketInstance.healPlayer(item.useEffect());
+    }
+    public useRemoveBleedingItem(item: RemoveBleedingItem): void {
+        this.socketInstance.healPlayer(item.useEffect());
+    }
+    public useTimeTravelItem(item: TimeTravelItem): void {
+        this.sceneEvents.emit(item.useEffect());
     }
 }
 
 export class BoostedItemVisitor implements IVisitor{
-    public useHealthItem(item : HealthItem, socketInstance : SocketController) : void {
-        socketInstance.healPlayer(item.useEffect() + 10);
+    private socketInstance : SocketController;
+    private sceneEvents : Phaser.Events.EventEmitter;
+
+    constructor(socketInstance : SocketController, sceneEvents : Phaser.Events.EventEmitter){
+        this.socketInstance = socketInstance;
+        this.sceneEvents = sceneEvents;
+    }
+
+    public useHealthItem(item : HealthItem) : void {
+        this.socketInstance.healPlayer(item.useEffect() + 10);
+    }
+    public useRemoveBleedingItem(item: RemoveBleedingItem): void {
+        this.socketInstance.healPlayer(item.useEffect());
+    }
+
+    public useTimeTravelItem(item: TimeTravelItem): void {
+        this.sceneEvents.emit(item.useEffect());
     }
 }
 
 export class SuperBoostedItemVisitor implements IVisitor{
-    public useHealthItem(item : HealthItem, socketInstance : SocketController) : void {
-        socketInstance.healPlayer(item.useEffect() + 20);
+    private socketInstance : SocketController;
+    private sceneEvents : Phaser.Events.EventEmitter;
+
+    constructor(socketInstance : SocketController, sceneEvents : Phaser.Events.EventEmitter){
+        this.socketInstance = socketInstance;
+        this.sceneEvents = sceneEvents;
+    }
+
+    public useHealthItem(item : HealthItem) : void {
+        this.socketInstance.healPlayer(item.useEffect() + 20);
+    }
+    public useRemoveBleedingItem(item: RemoveBleedingItem): void {
+        this.socketInstance.healPlayer(item.useEffect());
+    }
+
+    public useTimeTravelItem(item: TimeTravelItem): void {
+        this.sceneEvents.emit(item.useEffect());
     }
 }
